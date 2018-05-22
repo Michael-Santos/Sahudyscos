6353bfbafe901ae5dfff7bed9d2d7eac8b94a76d
Michael Santos <michael-santos@outlook.com.br>
2018-05-22 01:30:47 -0300

    Corrigidas referencia para css

--- CodeIgniter/application/views/index.php
+++ CodeIgniter/application/views/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sahudyscos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php echo base_url('css/') ?>style.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
@@ -16,7 +16,7 @@
         <nav class="navbar justify-content-center navbar-expand-lg navbar-dark bg-dark">
               
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <a class="navbar-brand justify-content-start" href="index.html"><img src="logo/logo.png" alt=""></a>
+                <a class="navbar-brand justify-content-start" href="index.html"><img src="<?php echo base_url('images/') ?>logo/logo.png" alt=""></a>
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item-active">
                        
@@ -48,13 +48,13 @@
 
         <div class="carousel-inner" style="background-color: black">
             <div class="carousel-item active">
-                <img src="banner/1.png" class="d-block mx-auto" alt="São legais mesmo">
+                <img src="<?php echo base_url('images/') ?>banner/1.png" class="d-block mx-auto" alt="São legais mesmo">
             </div>
             <div class="carousel-item">
-                <img src="banner/2.png" class="d-block mx-auto" alt="Vai por mim">
+                <img src="<?php echo base_url('images/') ?>banner/2.png" class="d-block mx-auto" alt="Vai por mim">
             </div>
             <div class="carousel-item">
-                <img src="banner/3.png" class="d-block mx-auto" alt="Pode confiar">
+                <img src="<?php echo base_url('images/') ?>banner/3.png" class="d-block mx-auto" alt="Pode confiar">
             </div>
         </div>
         
