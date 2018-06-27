12fbd8dfc98ed6834b6eba136cd7839defef1a42
donOnerb <breno.oliveira@dcomp.sor.ufscar.br>
2018-06-27 10:22:59 -0300

    Adicionado conexão com API Wikimedia  porém ainda não funciona

--- src/main/java/com/sahudyscos/web/controller/user/ArtistController.java
+++ src/main/java/com/sahudyscos/web/controller/user/ArtistController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.sahudyscos.web.entity.Artist;
 import com.sahudyscos.web.repository.ArtistRepository;
+import com.sahudyscos.web.service.WikiMediaService; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class ArtistController {
 
     @Autowired
     private ArtistRepository artistRepository;
 
+    @Autowired
+    private WikiMediaService wikiMediaService;
 
     @GetMapping("/artist")
     public String artist(@RequestParam(name="id") Integer id, Model model) {
@@ -25,6 +29,7 @@ public class ArtistController {
         currentArtist = artistRepository.findById(id.longValue());
 
         model.addAttribute("artist", currentArtist.get());
+        model.addAttribute("description", wikiMediaService.getDescription(currentArtist.get().getName()));
         return "artist";
     }
 
